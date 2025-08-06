4bbb78d378b4c01e161f9aeb6f56a3a64dc1adab
zihan19 <zihan19@illinois.edu>
2025-08-06 15:04:07 +0800

    111

--- prompt_with_lora.py
+++ prompt_with_lora.py
@@ -61,5 +61,5 @@ for dim in tqdm(["O", "C", "E", "A", "N"]):
 
     logger.info(f"Finished {trait}")
 
-with open("single_question_lora_prompt.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent=2)
+# with open("single_question_lora_prompt.json", "w", encoding="utf-8") as f:
+#     json.dump(data, f, ensure_ascii=False, indent=2)
